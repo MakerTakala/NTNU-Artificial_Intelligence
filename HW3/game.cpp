e2881f0ed4a85c5381a49d3e1acdc75522d0c6f9
Takala <dylan91011028@gmail.com>
2023-04-25 10:54:46 +0800

    add show board

--- HW3/game.cpp
+++ HW3/game.cpp
@@ -4,23 +4,30 @@
 
 using namespace std;
 
-void show_board(uint64_t board, const int n, const int m) {
-    for(int i = 0; i < n * m; i++) {
-        cout<<(board & 1)<<" ";
-        board = (board>>1);
-        if(i % n == n - 1) cout<<endl;
-    }
-}
+int n, m;
+void show_board(uint64_t board);
 fstream open_file(string file_path, const ios_base::openmode permittion);
-uint64_t read_board(fstream &file, int &n, int &m);
+uint64_t read_board(fstream &file);
 
 int main() {
-    int n, m;
     fstream in = open_file("./data/1.in", ios_base::in);
-    uint64_t board = read_board(in, n, m);
-    show_board(board, n, m);
+    uint64_t board = read_board(in);
+    show_board(board);
 
     return 0;
+}
+
+void show_board(uint64_t board) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout<<(board & 1)<<" ";
+            board = (board>>1);
+        }
+        for(int j = 0; j < 8 - m; j++) {
+            board = (board>>1);
+        }
+        cout<<endl;
+    }
 }
 
 fstream open_file(string file_path, const ios_base::openmode permittion) {
@@ -29,7 +36,7 @@ fstream open_file(string file_path, const ios_base::openmode permittion) {
     return file;
 }
 
-uint64_t read_board(fstream &file, int &n, int &m) {
+uint64_t read_board(fstream &file) {
     file>>n>>m;
     string input;
     for(int i = 0; i < n; i++) {
@@ -38,7 +45,6 @@ uint64_t read_board(fstream &file, int &n, int &m) {
             input = to_string(x) + input;
         }
         for(int j = 0, x; j < 8 - m; j++) {
-            file>>x;
             input = "0" + input;
         }
     }
