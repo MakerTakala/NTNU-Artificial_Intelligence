43fd98649e6a972ad45089cdf761c2fae5640f52
Takala <dylan91011028@gmail.com>
2023-05-04 17:34:16 +0800

    update

--- HW3/game.cpp
+++ HW3/game.cpp
@@ -1,51 +1,140 @@
 #include <bits/stdc++.h>
+#include <unistd.h>
 #include <fstream>
 #include <bitset>
 #include <vector>
 
 using namespace std;
 
-int n, m;
-class State {
-    public:
-        uint64_t board = 0;
-        pair<int, int> utility{INT_MIN, INT_MAX};
-        int amount[16] = {0};
-        
-        State(uint64_t _board) {
-            board = _board;
-        }
+uint64_t take_mask[16] = {
+    0xFFFFFFFFFFFFFF00, 
+    0xFFFFFFFFFFFF00FF, 
+    0xFFFFFFFFFF00FFFF, 
+    0xFFFFFFFF00FFFFFF, 
+    0xFFFFFF00FFFFFFFF, 
+    0xFFFF00FFFFFFFFFF, 
+    0xFF00FFFFFFFFFFFF, 
+    0x00FFFFFFFFFFFFFF,
+    0xFEFEFEFEFEFEFEFE,
+    0xFDFDFDFDFDFDFDFD, 
+    0xFBFBFBFBFBFBFBFB,
+    0xF7F7F7F7F7F7F7F7, 
+    0xEFEFEFEFEFEFEFEF, 
+    0xDFDFDFDFDFDFDFDF, 
+    0xBFBFBFBFBFBFBFBF,
+    0x7F7F7F7F7F7F7F7F
 };
 
-pair<int, int> maximize(State state);
+int n, m;
 
-void show_board(uint64_t board);
+pair<int, int> maximize(uint64_t board, int alpha, int beta, pair<int, int> get);
+pair<int, int> minimize(uint64_t board, int alpha, int beta, pair<int, int> get);
+
+void show(uint64_t board);
+int take_piece_amount(uint64_t board, uint64_t mask);
+uint64_t remove(uint64_t board, uint64_t mask);
 fstream open_file(string file_path, const ios_base::openmode permittion);
 uint64_t read_board(fstream &file);
 
 
-
 int main() {
     fstream in = open_file("./data/1.in", ios_base::in);
-    uint64_t board = read_board(in);
-    show_board(board);
+    uint64_t init_board = (read_board(in));
+    in.close();
+    
+    pair<int, int> get{0, 0};
+    vector<int> selects;
+
+    int turn = 0;
+    while(init_board) {
+        show(init_board);
+
+        pair<int, int> ans = maximize(init_board, INT_MIN, INT_MAX, {0, 0});
+        int utility = ans.first, step = ans.second;
+
+        cout<<"STEP: "<<step<<endl<<"Utility: "<<utility<<endl;
+        selects.push_back(step);
+
+        uint64_t next_board = remove(init_board, take_mask[step]);
+        int take = take_piece_amount(init_board, next_board);
+
+        if(turn == 0) {
+            get.first += take;
+        }
+        else {
+            get.second += take;
+        }
+        turn ^= 1;
+
+        init_board = next_board;
+        
+    }
+
+    cout<<endl<<endl<<"FIN:"<<endl;
+    for(int x : selects) {
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    cout<<"GET: "<<get.first<<" "<<get.second<<endl;
+
+    
 
     return 0;
 }
 
-pair<int, int> maximize(State state) {
-    if(state.board == 0) return {0, 0};
-    int max_choose = 0, choose = 0;
-    for(int i = 0; i < n + m; i++) {
-        if(max_choose < state.amount[i]) {
-            max_choose = state.amount[i];
-            choose = i;
+pair<int, int> maximize(uint64_t board, int alpha, int beta, pair<int, int> get) {
+    //usleep(500 * 1000);
+    if(board == 0) return {get.first - get.second, -1};
+
+    int max_utility = INT_MIN, max_select = -1;
+    for(int i = 0; i < 16; i++) {
+        uint64_t next_board = remove(board, take_mask[i]);
+        int take = take_piece_amount(board, next_board);
+        if(take == 0) continue;
+
+        pair<int, int> next = minimize(next_board, alpha, beta, {get.first + take, get.second});
+        int next_utility = next.first;
+
+        if(next_utility > max_utility) {
+            max_utility = next_utility;
+            max_select = i;
         }
+        
+        if(max_utility >= beta) break;
+
+        alpha = max(alpha, max_utility);
     }
-    return {0, 0};
+    return {max_utility, max_select};
 }
 
-void show_board(uint64_t board) {
+pair<int, int> minimize(uint64_t board, int alpha, int beta, pair<int, int> get) {
+    //usleep(500 * 1000);
+    if(board == 0) return {get.first - get.second, -1};
+
+    int min_utility = INT_MAX, min_select = -1;
+    for(int i = 0; i < 16; i++) {
+        uint64_t next_board = remove(board, take_mask[i]);
+        int take = take_piece_amount(board, next_board);
+        if(take == 0) continue;
+        
+
+        pair<int, int> next = maximize(next_board, alpha, beta, {get.first, get.second + take});
+        int next_utility = next.first;
+
+        if(next_utility < min_utility) {
+            min_utility = next_utility;
+            min_select = i;
+        }
+
+        if(min_utility <= alpha) break;
+
+        beta = min(beta, min_utility);
+    }
+    return {min_utility, min_select};
+}
+
+
+void show(uint64_t board) {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             cout<<(board & 1)<<" ";
@@ -56,6 +145,22 @@ void show_board(uint64_t board) {
         }
         cout<<endl;
     }
+    cout<<endl;
+}
+
+uint64_t remove(uint64_t board, uint64_t mask){
+    return board & mask;
+}
+
+int take_piece_amount(uint64_t board, uint64_t mask) {
+    int count = 0;
+    int n = board ^ mask;
+    while (n)
+    {
+        n = n & (n - 1);
+        count++;
+    }
+    return count;
 }
 
 fstream open_file(string file_path, const ios_base::openmode permittion) {
